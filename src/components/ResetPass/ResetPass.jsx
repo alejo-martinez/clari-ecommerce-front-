89f26364d3a4c459272343f612dc03cda3720c8d
alejo-martinez <alejoomartinex11@gmail.com>
2023-12-28 08:52:01 -0300

    component added

--- src/components/ResetPass/ResetPass.jsx
+++ src/components/ResetPass/ResetPass.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 
 import './ResetPass.css';
 
@@ -16,25 +16,25 @@ function ResetPass() {
     const [pass, setPass] = useState('');
     const [passCheck, setPassCheck] = useState('');
 
-    const handlePass = ({target: {value}})=>{
+    const handlePass = ({ target: { value } }) => {
         setPass(value);
     };
 
-    const handlePassCheck = ({target: {value}})=>{
+    const handlePassCheck = ({ target: { value } }) => {
         setPassCheck(value);
     };
 
-    const sendData = async()=>{
-        if(pass !== passCheck) setError('Las contraseñas no coinciden.');
-        else{
+    const sendData = async () => {
+        if (pass !== passCheck) setError('Las contraseñas no coinciden.');
+        else {
             const resp = await updatePassword(uid, pass);
-            if(resp.status === 'success'){
+            if (resp.status === 'success') {
                 toast.success(resp.message, { position: "top-center", autoClose: 1300, hideProgressBar: true, closeOnClick: true, closeButton: true, pauseOnHover: false });
                 setTimeout(() => {
                     navigation('/login');
                 }, 2300);
             }
-            if(resp.error){
+            if (resp.error) {
                 toast.error(resp.error, { position: "top-center", autoClose: 1300, hideProgressBar: true, closeOnClick: true, closeButton: true, pauseOnHover: false })
             }
         }
@@ -44,18 +44,18 @@ function ResetPass() {
 
     return (
         <div>
-                    <div>
-                        <div>
-                            <label>Contraseña</label>
-                            <input type="password" name="pass" onChange={handlePass}/>
-                        </div>
-                        <div>
-                            <label>Confirmar contraseña</label>
-                            <input type="password" name="passCheck" onChange={handlePassCheck}/>
-                        </div>
-                        <button onClick={()=> sendData()}>Actualizar</button>
-                        { error && <p>{error}</p> }
-                    </div>
+            <div>
+                <div>
+                    <label>Contraseña</label>
+                    <input type="password" name="pass" onChange={handlePass} />
+                </div>
+                <div>
+                    <label>Confirmar contraseña</label>
+                    <input type="password" name="passCheck" onChange={handlePassCheck} />
+                </div>
+                <button onClick={() => sendData()}>Actualizar</button>
+                {error && <p>{error}</p>}
+            </div>
         </div>
     );
 };
